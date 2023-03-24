c4799f0009ce01b269da28cc3bd219920b032170
samehsebai <69249820+samehsebai@users.noreply.github.com>
2023-03-24 01:37:12 +0100

    Delete src directory

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-module.exports = {
-    DB_HOST: process.env.DB_HOST,
-    TOKEN_SECRET:process.env.TOKEN_SECRET,
-    EMAIL:process.env.EMAIL,
-    EMAIL_PASS:process.env.EMAIL_PASS
-};
