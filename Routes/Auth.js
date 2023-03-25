03082311260868863d2ea9e3efc86ad39518753c
ahlem tbini <74101630+ahlemtbini@users.noreply.github.com>
2023-03-25 04:53:19 +0100

    Update Auth.js

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -1,21 +1,18 @@
-const express=require('express');
+const express = require('express');
 const passport = require('passport');
-const {forgetpassword, resetPassword } = require('../Controllers/Auth/Forget_Reset_password');
+const { forgetpassword, resetPassword , UpdateUser } = require('../Controllers/Auth/Forget_Reset_password');
 const { login } = require('../Controllers/Auth/Login');
 const { register } = require('../Controllers/Auth/Register');
-const authRole = require('../Passport/RoleAllowed')
-const router=express.Router()
+const authRole = require('../Passport/RoleAllowed');
+const router = express.Router();
 
-router.post('/register', 
-passport.authenticate('bearer', { session: false }),authRole("ADMIN"),register)
-router.post('/login',login)
-router.get('/profile', 
-  passport.authenticate('bearer', { session: false }),
-  function(req, res) {
-    res.json(req.user);
-  });
-router.post('/forgetPassword',forgetpassword)
-router.put('/resetPassword/:token',resetPassword)
+router.post('/register', passport.authenticate('bearer', { session: false }), authRole("ADMIN"), register);
+router.post('/login', login);
+router.get('/profile', passport.authenticate('bearer', { session: false }), function(req, res) {
+  res.json(req.user);
+});
+router.post('/forgetPassword', forgetpassword);
+router.put('/resetPassword/:token', resetPassword);
+router.put('/updateuser/:iduser', UpdateUser);
 
-
-module.exports=router
+module.exports = router;
