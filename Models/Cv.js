bd58659b9180d5a7ff3d91e9d7cbb3b66e504fa6
samehsebai <69249820+samehsebai@users.noreply.github.com>
2023-04-04 21:40:06 +0100

    Update Cv.js

--- Models/Cv.js
+++ Models/Cv.js
@@ -4,7 +4,11 @@ mongoose.Promise = global.Promise;
 
 const CvSchema = new Schema(
     {
-       
+        firstName: { type: String, required: [true, 'Prénom obligatoire!'] },
+        lastName: { type: String, required: [true, 'Nom de famille obligatoire!'] },
+        email: { type: String, required: [true, 'E-mail obligatoire!'] },
+        address: { type: String, required: [true, 'Adresse obligatoire!'] },
+        Specialite: { type: String },
         stages: { type: String, required: [true, 'stages obligatoire!'] },
         formations: { type: String, required: [true, 'formations obligatoire!'] },
         compétences: { type: String, required: [true, ' compétences obligatoire!'] },
