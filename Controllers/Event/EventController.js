c33d5dbdefdff6fc0994aa043bcbc413dc83ae68
samehsebai <69249820+samehsebai@users.noreply.github.com>
2023-04-04 21:38:26 +0100

    Update EventController.js

--- Controllers/Event/EventController.js
+++ Controllers/Event/EventController.js
@@ -1,84 +1,82 @@
 const Event = require('../../Models/Event')
 
-exports.CreateEvent = async (req , res)=>{
+exports.CreateEvent = async (req, res) => {
 
     try {
-        
-    const Result = await Event.create(req.body)
-    res.send(Result)
-    
-    
+        console.log(req.body)
+        const Result = await Event.create(req.body)
+        const Result2 = Result.save();
+        res.send(Result2)
     } catch (error) {
-    
+        console.log(error)
         res.status(500).send(error)
     }
-    
-    }
-
-
-exports.FetchEvent = async (req , res)=>{
-
-try {
-    
-const Result = await Event.find({})
-
-res.send(Result)
-
-
-} catch (error) {
-
-    res.status(500).send(error)
-}
 
 }
 
 
-exports.FetchEventById = async (req , res)=>{
+exports.FetchEvent = async (req, res) => {
 
     try {
-        
-    const Result = await Event.findById(req.params.idEvent)
-    
-    res.send(Result)
-    
-    
+
+        const Result = await Event.find({})
+
+        res.send(Result)
+
+
     } catch (error) {
-    
+
         res.status(500).send(error)
     }
-    
+
+}
+
+
+exports.FetchEventById = async (req, res) => {
+
+    try {
+
+        const Result = await Event.findById(req.params.idEvent)
+
+        res.send(Result)
+
+
+    } catch (error) {
+
+        res.status(500).send(error)
     }
 
-    exports.UpdateEvent= async (req , res)=>{
+}
 
-        try {
-            
-        const Result = await Event.findByIdAndUpdate(req.params.idEvent , req.body)
+exports.UpdateEvent = async (req, res) => {
+
+    try {
+
+        const Result = await Event.findByIdAndUpdate(req.params.idEvent, req.body)
 
         const Resultupdate = await Event.findById(req.params.idEvent)
-        
+
         res.send(Resultupdate)
-        
-        
-        } catch (error) {
-        
-            res.status(500).send(error)
-        }
-        
-        }
 
 
-        exports.DeleteEvent = async (req , res )=>
-        {
-            try {
+    } catch (error) {
 
-                const Result = await Event.findByIdAndDelete(req.params.idEvent) 
+        res.status(500).send(error)
+    }
 
-                
-                res.status(200).send('Event deleted with success')
-            } catch (error) {
-                
+}
 
-                res.status(500).send('error serveur')
-            }
-        }
+
+exports.DeleteEvent = async (req, res) => {
+    try {
+
+        const Result = await Event.findByIdAndDelete(req.params.idEvent)
+
+
+        res.status(200).send('Event deleted with success')
+    } catch (error) {
+
+
+        res.status(500).send('error serveur')
+    }
+}
