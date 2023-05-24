0f5d5d1432cf340ec345bbf9dc53cbef9ea4f168
sameh <sameh.sebai.0104@gmail.com>
2023-05-24 12:03:18 +0200

    fix

--- Controllers/PFA/PFAController.js
+++ Controllers/PFA/PFAController.js
@@ -1,85 +1,88 @@
-const PFA = require('../../Models/PFA')
+const PFA = require("../../Models/PFA");
 
-exports.CreatePFA = async (req , res)=>{
+exports.CreatePFA = async (req, res) => {
+  try {
+    const pickedBody = req.body;
+    pickedBody.statue = "pending";
+    const Result = await PFA.create(req.body);
 
-    try {
-        
-    const Result = await PFA.create(req.body)
-    
-    res.send(Result)
-    
-    
-    } catch (error) {
-    
-        res.status(500).send(error)
+    res.send(Result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.FetchPFA = async (req, res) => {
+  try {
+    let query = {};
+    const technologie = req.query.Technologie;
+    const idEns = req.query.idEns;
+    if (technologie) {
+      query = {
+        Technologie: technologie,
+      };
     }
-    
+    if (idEns) {
+      query = {
+        createdBy: idEns,
+      };
     }
+    const Result = await PFA.find(query).populate("createdBy");
+    res.send(Result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
+exports.FetchPFAById = async (req, res) => {
+  try {
+    const Result = await PFA.findById(req.params.idPFA);
 
-exports.FetchPFA = async (req , res)=>{
+    res.send(Result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-try {
-    
-const Result = await PFA.find({})
+exports.UpdatePFA = async (req, res) => {
+  try {
+    const Result = await PFA.findByIdAndUpdate(req.params.idPFA, req.body);
 
-res.send(Result)
+    const Resultupdate = await PFA.findById(req.params.idPFA);
 
+    res.send(Resultupdate);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-} catch (error) {
+exports.DeletePFA = async (req, res) => {
+  try {
+    const Result = await PFA.findByIdAndDelete(req.params.idPFA);
 
-    res.status(500).send(error)
-}
+    res.status(200).send("PFA deleted with success");
+  } catch (error) {
+    res.status(500).send("error serveur");
+  }
+};
 
-}
-
-
-exports.FetchPFAById = async (req , res)=>{
-
-    try {
-        
-    const Result = await PFA.findById(req.params.idPFA)
-    
-    res.send(Result)
-    
-    
-    } catch (error) {
-    
-        res.status(500).send(error)
-    }
-    
-    }
-
-    exports.UpdatePFA= async (req , res)=>{
-
-        try {
-            
-        const Result = await PFA.findByIdAndUpdate(req.params.idPFA , req.body)
-
-        const Resultupdate = await PFA.findById(req.params.idPFA)
-        
-        res.send(Resultupdate)
-        
-        
-        } catch (error) {
-        
-            res.status(500).send(error)
-        }
-        
-        }
-
-
-        exports.DeletePFA = async (req , res )=>
-        {
-            try {
-
-                const Result = await PFA.findByIdAndDelete(req.params.idPFA) 
-
-                
-                res.status(200).send('PFA deleted with success')
-            } catch (error) {
-                
-
-                res.status(500).send('error serveur')
-            }
-        }
+exports.FetchPFAByIdEns = async (req, res) => {
+  try {
+    const Result = await PFA.find({
+      createdBy: req.params.idEns,
+    });
+    res.json(Result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+exports.FetchPFAStudents = async (req, res) => {
+  try {
+    const Result = await PFA.find({
+      statue: "accepted",
+    });
+    res.json(Result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
--- Routes/CvRoutes.js
+++ Routes/CvRoutes.js
@@ -1,18 +1,39 @@
-const express=require('express');
-const authRole = require('../Passport/RoleAllowed')
+const express = require("express");
+const authRole = require("../Passport/RoleAllowed");
 
+const {
+  FetchCv,
+  FetchCvByStudentId,
+  UpdateCv,
+  DeleteCv,
+  CreateCv,
+  FetchCvById,
+} = require("../Controllers/Cv/CvController");
+const passport = require("passport");
 
-const { FetchCv, FetchCvByStudentId, UpdateCv, DeleteCv, CreateCv } = require('../Controllers/Cv/CvController');
-const passport = require('passport');
+const router = express.Router();
+router.post(
+  "/CreateCv",
+  passport.authenticate("bearer", { session: false }),
+  CreateCv
+);
+router.get("/Cv", passport.authenticate("bearer", { session: false }), FetchCv);
+router.get(
+  "/CvByStudent/:id_st",
+  passport.authenticate("bearer", { session: false }),
+  FetchCvByStudentId
+);
+router.get("/CvByid/:idCv", FetchCvById);
+router.put(
+  "/Cv/:idCv",
+  passport.authenticate("bearer", { session: false }),
+  UpdateCv
+);
+router.delete(
+  "/Cv/:idCv",
+  passport.authenticate("bearer", { session: false }),
+  authRole("ADMIN", "Enseignant"),
+  DeleteCv
+);
 
-const router=express.Router()
-router.post('/CreateCv',  passport.authenticate('bearer', { session: false }) , CreateCv)
-router.get('/Cv',  passport.authenticate('bearer', { session: false }), FetchCv)
-router.get('/CvByStudent/:id_st',  passport.authenticate('bearer', { session: false }), FetchCvByStudentId)
-router.put('/Cv/:idCv',  passport.authenticate('bearer', { session: false }) , UpdateCv)
-router.delete('/Cv/:idCv' ,  passport.authenticate('bearer', { session: false }), authRole("ADMIN", "Enseignant"), DeleteCv)
-
-
-
-
-module.exports=router
+module.exports = router;
