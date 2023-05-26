51111d8f078f085babc7600e0c8164868433c621
ahlem tbini <74101630+ahlemtbini@users.noreply.github.com>
2023-05-27 00:27:06 +0100

    Update Auth.js

--- Routes/Auth.js
+++ Routes/Auth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const passport = require('passport');
 const { forgetpassword, resetPassword , UpdateUser } = require('../Controllers/Auth/Forget_Reset_password');
 const { login } = require('../Controllers/Auth/Login');
-const { register } = require('../Controllers/Auth/Register');
+const { register, registerAlumni } = require('../Controllers/Auth/Register');
 const authRole = require('../Passport/RoleAllowed');
 const router = express.Router();
-
+router.post('/registerAlumni',  registerAlumni);
 router.post('/register',  register);
 router.post('/login', login);
 router.get('/profile', passport.authenticate('bearer', { session: false }), function(req, res, next) {
